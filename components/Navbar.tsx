279d648824850da3608b151542c8d82002fdef74
waldo <luis.betancourt.dev@gmail.com>
2023-05-08 18:36:41 -0500

    Started working on mobile menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { NavItem } from "@ts/interfaces/app_interfaces";
 
+import Container from "@components/Container";
+
 interface Props {
     leftNavItems: NavItem[];
     rightNavItems: NavItem[];
@@ -16,10 +18,12 @@ const NavItem = ({ href, label }: NavItem) => (
 
 const Navbar = ({ leftNavItems, rightNavItems }: Props) => {
     return (
-        <nav className="bg-red-50">
-            <div className='_container flex-center'>
+        <nav className="bg-slate-100">
+
+            {/* desktop navigation */}
+            <Container>
                 <Link href="/" className="pr-3">
-                    <Image src="/hPower.svg" alt="Harrison Hydra-Gen" width={30} height={30} />
+                    <Image src="/hPower.svg" alt="Harrison Hydra-Gen Logo" width={30} height={30} />
                 </Link>
                 <ul className="mr-auto flex">
                     {leftNavItems.map(({ href, label }) => (
@@ -31,9 +35,25 @@ const Navbar = ({ leftNavItems, rightNavItems }: Props) => {
                         <NavItem key={`${href}${label}`} href={href} label={label} />
                     ))}
                 </ul>
-            </div>
+            </Container>
+
+            {/* mobile navigation */}
+            <Container>
+                <Image
+                    src="/logo.png"
+                    alt="Harrison Hydra-Gen logo"
+                    width={30}
+                    height={30}
+                />
+                <ul className="flex">
+                    {rightNavItems.map(({ href, label }) => (
+                        <NavItem key={`${href}${label}`} href={href} label={label} />
+                    ))}
+                </ul>
+            </Container>
         </nav>
     )
 }
 
 export default Navbar;
+
