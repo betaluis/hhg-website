a717657e86ea58909b8d382200815124b933e47b
waldo <luis.betancourt.dev@gmail.com>
2023-05-09 15:02:55 -0500

    Menu fade in

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -2,6 +2,7 @@
  
 import React, { useState, useEffect } from 'react';
 import MenuIcon from '@/components/icons/MenuButton';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const MobileMenu = () => {
 
@@ -20,11 +21,27 @@ const MobileMenu = () => {
             <div onClick={() => setIsOpen(prev => !prev)}>
                 <MenuIcon isOpen={isOpen} />
             </div>
-            {isOpen && (
-                <div className="absolute top-[49px] left-0 w-full h_screen bg-blue-100">
-                    hello
-                </div>
-            )}
+            <AnimatePresence>
+                {isOpen && (
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.2, ease: 'easeInOut' }}
+                    >
+                        <div className="absolute top-[49px] left-0 w-full h_screen bg-blue-100">
+                            <motion.div
+                                initial={{ opacity: 0, y: -20 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0 }}
+                                transition={{ duration: 0.2, ease: 'easeInOut' }}
+                            >
+                                <div className="pt-40">hello</div>
+                            </motion.div>
+                        </div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </>
     )
 }
