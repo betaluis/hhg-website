bb4917794407ed4d7aad0099de25629d5fe847e0
waldo <luis.betancourt.dev@gmail.com>
2023-05-09 15:03:22 -0500

    Placeholder hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 export default function Home() {
     return (
         <main className="_container">
-            <div className="h_screen bg-red-400 flex-center">
-                <h1 className="">Hero Section</h1>
+            <div className="h_screen">
+                <h1 className="text-2xl font-bold pt-14">Hero Section</h1>
+                <p className="pt-4">Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit.</p>
+                <div className="pt-4">
+                    <button className="bg-black px-4 py-2 text-white rounded-full">Learn More</button>
+                </div>
             </div>
         </main>
     )
