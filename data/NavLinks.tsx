980d0cb8c7f532e34089300e608c77d1002a6b1a
waldo <luis.betancourt.dev@gmail.com>
2023-05-12 15:58:51 -0500

    Mobile menu industry links and icons updated

--- data/NavLinks.tsx
+++ data/NavLinks.tsx
@@ -1,4 +1,4 @@
-import { ScissorLift } from "@/components/icons/IndustryIcons";
+import { ScissorLift, FireTruck, Ambulance, Construction, OilGas, Military } from "@/components/icons/IndustryIcons";
 
 export const leftNavItems: NavItem[] = [
     { href: "/products", label: "Products" },
@@ -12,5 +12,10 @@ export const rightNavItems: NavItem[] = [
 ];
 
 export const industriesNavItems: NavItem[] = [
-    { href: "/", label: "Industry", icon: <ScissorLift /> },
+    { href: "/mewp", label: "MEWP", icon: <ScissorLift /> },
+    { href: "/fire-and-rescue", label: "Fire & Rescue", icon: <FireTruck /> },
+    { href: "/ambulance", label: "Ambulance", icon: <Ambulance /> },
+    { href: "/construction", label: "Construction", icon: <Construction /> },
+    { href: "/oilGas", label: "Oil & Gas", icon: <OilGas /> },
+    { href: "/military", label: "Military", icon: <Military /> },
 ]
