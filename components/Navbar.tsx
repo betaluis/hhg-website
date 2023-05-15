b1cccd2e9e53e8451c126a4b2d4b7ea5d4121187
waldo <luis.betancourt.dev@gmail.com>
2023-05-15 15:48:36 -0500

    Fixed mobile menu height

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -38,7 +38,7 @@ const NavList = ({ links, mr, ml }: NavListProps): JSX.Element  => (
 const Navbar = ({ leftNavItems, rightNavItems }: Props) => {
 
     return (
-        <nav className="bg-slate-100">
+        <nav className="">
 
             {/* desktop navigation */}
             <Container>
