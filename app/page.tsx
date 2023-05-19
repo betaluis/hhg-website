84ee1776023a4226b02b4b926b763e7913b58a16
waldo <luis.betancourt.dev@gmail.com>
2023-05-19 13:38:20 -0500

    Mobile styles for hero text

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Container from "@/components/Container";
 import Button from "@/components/Button";
-import { Text } from "@/components/typography/Text";
+import { Heading, Text } from "@/components/typography/Text";
 
 export default function Home() {
     return (
@@ -9,13 +9,17 @@ export default function Home() {
             <section className="bg-green-600 relative">
                 <Container>
                     <div className="py-24">
-                        <h1 className="text-slate-100 font-bold text-[2rem] text-center md:text-[2rem] lg:text-[4rem] max-w-[800px]">
-                            We are mobile power.
-                        </h1>
-                        <Text color="light">
-                            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-                        </Text>
-                        <div className="mt-8 flex gap-4">
+                        <div className="pt-8 sm:pt-4 lg:pt-0">
+                            <Heading size="h1" color="light">
+                                We are mobile power.
+                            </Heading>
+                        </div>
+                        <div className="py-4 max-w-lg m-auto sm:m-0">
+                            <Text color="light">
+                                Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+                            </Text>
+                        </div>
+                        <div className="mt-2 flex flex-col sm:flex-row gap-4">
                             <Button label="Products" href="/products" type="secondary" className="bg-slate-600 px-8 text-xl bg-gradient-to-t from-slate-700 drop-shadow-[0_2px_0_rgba(30,40,20,1)] hover:from-slate-600" />
                             <Button label="Get in touch" href="/" type="secondary" className="bg-green-400 px-8 text-xl bg-gradient-to-t from-green-500 drop-shadow-[0_2px_0_rgba(22,101,52,1)] hover:from-green-400" />
                         </div>
--- components/typography/Text.tsx
+++ components/typography/Text.tsx
@@ -1,11 +1,12 @@
 interface TextProps {
     children: React.ReactNode;
     color?: "light" | "dark";
+    classes?: string;
 }
 
-export const Text = ({ children, color = "light" }: TextProps) => {
+export const Text = ({ children, color = "light", classes }: TextProps) => {
     return (
-        <p className={`text-center sm:text-left text-sm sm:text-base lg:text-lg ${color === "light" ? "text-slate-100" : "text-slate-900"}`}>
+        <p className={`text-center sm:text-left text-sm sm:text-base lg:text-lg ${color === "light" ? "text-slate-100" : "text-slate-900"} ${classes}`}>
             {children}
         </p>
     )
@@ -15,17 +16,17 @@ interface HeadingProps extends TextProps{
     size: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 }
 // TODO: Create the rest
-export const Heading = ({ children, size,  color = "light" }: HeadingProps) => {
+export const Heading = ({ children, size,  color = "light", classes }: HeadingProps) => {
     switch (size) {
         case "h1":
             return ( 
-                <h1 className={`text-center sm:text-left text-2xl sm:text-3xl lg:text-4xl font-bold ${color === "light" ? "text-slate-100" : "text-slate-900"}`}>
+                <h1 className={`text-center sm:text-left text-3xl sm:text-4xl lg:text-5xl font-black ${color === "light" ? "text-slate-100" : "text-slate-900"} ${classes}`}>
                     {children}
                 </h1>
             );
         case "h2":
             return (
-                <h2 className={`text-center sm:text-left text-xl sm:text-2xl lg:text-3xl font-bold ${color === "light" ? "text-slate-100" : "text-slate-900"}`}>
+                <h2 className={`text-center sm:text-left text-xl sm:text-2xl lg:text-3xl font-bold ${color === "light" ? "text-slate-100" : "text-slate-900"} ${classes}`}>
                     {children}
                 </h2>
             );
