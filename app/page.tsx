58ecb194ccee4b0733d2427593e71ba542dfd4a2
waldo <luis.betancourt.dev@gmail.com>
2023-05-22 13:00:05 -0500

    removed imports

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 import Container from "@/components/Container";
 import Products from "@/data/Products";
-import Hero from "@/sections/home/Hero";
-import ProductsSection from "@/sections/home/ProductsSection";
 import { Heading, Text } from "@/components/typography/Text";
 import Button from "@/components/Button";
 
