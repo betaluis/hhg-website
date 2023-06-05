2b0ba9ef62ac052e58f933ce0e6b18989e945b31
waldo <luis.betancourt.dev@gmail.com>
2023-06-05 15:34:09 -0500

    Loading: Testing load page

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+
+const loading = () => {
+  return (
+    <h1 className="h-screen w-full bg-red-500">LOADING</h1>
+  )
+}
+
+export default loading
---
+++ app/products/loading.tsx
@@ -0,0 +1,7 @@
+const loading = () => {
+  return (
+    <div className="h-screen w-screen bg-red-800">loading</div>
+  )
+}
+
+export default loading
