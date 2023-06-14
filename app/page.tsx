450f9d6d99755b2641e89e747496c678cc152208
waldo <luis.betancourt.dev@gmail.com>
2023-06-14 13:48:12 -0500

    test still

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
 import Products from "@/data/Products";
 import ProductCards from "@/components/ProductCards";
 import { CompanyAgeSection, FeaturesSection, Hero, TestimonialSection } from "@/sections/home";
+import BlurEffect from "@/components/BlurEffect";
+import Container from "@/components/Container";
 
 export default function Home() {
 
     return (
         <>
-            <Hero />
+            {/* <Hero /> */}
+        <section className="bg-bkg-green relative">
+            <BlurEffect>
+                <Container classes="bg_people">
+                    <div className="flex justify-center sm:justify-start">
+                        <div className="pt-24 pb-44 sm:py-32 lg:py-14">
+                            <h1 className="text-white pt-24">Hello</h1>
+                        </div>
+                    </div>
+                </Container>
+            </BlurEffect>
+        </section>
+
 
             <ProductCards products={Products} />
 
